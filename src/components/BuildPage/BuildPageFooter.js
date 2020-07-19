cae0652ccee17eb25a2ca4043a8f1653f80efb04
UtterAirmore <68033078+UtterAirmore@users.noreply.github.com>
2020-07-19 19:15:09 +0200

    footer

--- src/components/BuildPage/BuildPageFooter.js
+++ src/components/BuildPage/BuildPageFooter.js
@@ -1,13 +1,16 @@
 import React from "react"
 
-import {Row, Col, Button, Card} from "react-bootstrap"
+import {Row, Col, Badge} from "react-bootstrap"
 
 import styles from "./footer.module.css"
 
 function BuildPageFooter() {
     return(
         <footer className={styles.footer}>
-            hello
+                <Row className={styles.content}>
+                    <Col xl={7} sm ={7}></Col>
+                    <Col xl={5} sm={5}><Badge className="p-3" variant="primary">Price</Badge></Col>
+                </Row>
         </footer>
     )
 }
